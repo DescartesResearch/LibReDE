b2438436c05c64b881a2d169ae6d625ff396ce3d
s_salimi <s_salimi@9e42b895-fcda-4063-8a3b-11be15eb1bbd>
2012-12-21 09:41:42 +0000

    added NNLS Test
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/descartes/code/redeem/trunk@9609 9e42b895-fcda-4063-8a3b-11be15eb1bbd

---
+++ Redeem/Estimation Library/edu.kit.ipd.descartes.redeem.nnls.java/src/edu/kit/ipd/descartes/redeem/nnls/test/Test.java
@@ -0,0 +1,15 @@
+package edu.kit.ipd.descartes.redeem.nnls.test;
+
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		TestScenarios.RunTest();		
+
+	}
+
+}
