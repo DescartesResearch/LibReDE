2e10fd11137222578753f24fdc14f9a0f5a85c6c
s_salimi <s_salimi@9e42b895-fcda-4063-8a3b-11be15eb1bbd>
2013-01-31 23:12:59 +0000

    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/descartes/code/redeem/trunk@10077 9e42b895-fcda-4063-8a3b-11be15eb1bbd

---
+++ Redeem/Estimation Library/edu.kit.ipd.descartes.redeem.nnls.java.tests/src/edu/kit/ipd/descartes/redeem/nnls/tests/VectorAssert.java
@@ -0,0 +1,32 @@
+package edu.kit.ipd.descartes.redeem.nnls.tests;
+
+
+
+import org.fest.assertions.api.AbstractAssert;
+import org.fest.assertions.api.Assertions;
+import org.fest.assertions.data.Offset;
+import edu.kit.ipd.descartes.linalg.Vector;
+
+public class VectorAssert extends AbstractAssert<VectorAssert, Vector> {
+	
+	protected VectorAssert(Vector actual) {
+		super(actual, VectorAssert.class);
+	}
+
+	public VectorAssert isEqualTo(Vector expected, Offset<Double> delta) {
+		Assertions.assertThat(actual.rows())
+			.overridingErrorMessage("Expected vector row count to be <%s> but was <%s>", expected.rows(), actual.rows())
+			.isEqualTo(expected.rows());
+		for (int i = 0; i < actual.rows(); i++) {			
+			Assertions.assertThat(actual.get(i))
+				.overridingErrorMessage("Expected vector to be <%s> but was <%s>", expected.toString(), actual.toString())
+				.isEqualTo(expected.get(i), delta);
+		}
+		return this;
+	}
+
+	public static VectorAssert assertThat(Vector actual) {
+		return new VectorAssert(actual);
+	}
+	
+}
