193640436eaa9719e22bfd2ee42fba16e23b2df7
s_salimi <s_salimi@9e42b895-fcda-4063-8a3b-11be15eb1bbd>
2013-01-31 21:03:35 +0000

    - added simple regression
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/descartes/code/redeem/trunk@10070 9e42b895-fcda-4063-8a3b-11be15eb1bbd

--- Redeem/Estimation Library/edu.kit.ipd.descartes.redeem.nnls.java/src/edu/kit/ipd/descartes/redeem/nnls/LeastSquares.java
+++ Redeem/Estimation Library/edu.kit.ipd.descartes.redeem.nnls.java/src/edu/kit/ipd/descartes/redeem/nnls/LeastSquares.java
@@ -1,32 +1,59 @@
 package edu.kit.ipd.descartes.redeem.nnls;
 
+import cern.colt.matrix.DoubleMatrix2D;
+import cern.colt.matrix.linalg.Algebra;
 import com.sun.jna.Memory;
 import com.sun.jna.ptr.DoubleByReference;
 import com.sun.jna.ptr.IntByReference;
 import edu.kit.ipd.descartes.linalg.Matrix;
 import edu.kit.ipd.descartes.linalg.Vector;
 import edu.kit.ipd.descartes.redeem.nnls.backend.NNLSLibrary;
+
 /**
+ * This class implements Least-Squares (NNLS) algorithm.
  * 
  * @author Mehran Saliminia
- *
+ * 
  */
 public class LeastSquares {
+
+	private final int SIZE_OF_DOUBLE = 8;
+	private final int SIZE_OF_INT = 8;
+	private static final cern.colt.matrix.DoubleFactory2D Factory2D = cern.colt.matrix.DoubleFactory2D.dense;
 
 	public LeastSquares() {
 
 	}
 
-	// Estimates an ordinary least squares regression model with one independent
-	// variable.
-	// y = intercept + slope * x
-	public double simpleRegression(Matrix data) throws Exception {
-		throw new Exception("Not implemented!");
+	/**
+	 * This method uses colt library to solves A * X = B.
+	 * 
+	 * @param A
+	 * @param B
+	 * @return X; a new independent matrix; solution if A is square, least
+	 *         squares solution otherwise.
+	 * 
+	 */
+	public Matrix solve(Matrix A, Matrix B) {
+		Algebra alg = new Algebra();
+		DoubleMatrix2D a = Factory2D.make(A.toArray());
+		DoubleMatrix2D b = Factory2D.make(B.toArray());
+		DoubleMatrix2D x = alg.solve(a, b);
+		Matrix X = Matrix.matrix(x.toArray());
+		return X;
 	}
 
-	// Implements the Non-Negative Least-Squares (NNLS) algorithm
-	// We are initially given the m*n matrix E and the m-vector F
-	// Minimize ||Ex-F|| subject to x >= 0
+	/**
+	 * This method implements the Non-Negative Least-Squares (NNLS) algorithm.
+	 * We are initially given the m*n matrix E and the m-vector F. Minimize
+	 * ||Ex-F|| subject to x >= 0.
+	 * 
+	 * @param E
+	 *            m*n matrix
+	 * @param F
+	 *            m-vector
+	 * @return the solution Vector
+	 */
 	public Vector nnls(Matrix E, Vector F) {
 		try {
 			System.loadLibrary("NNLS");
@@ -35,19 +62,20 @@ public class LeastSquares {
 
 			// Check inputs
 			if (E == null || F == null || E.rows() != F.rows())
-				throw new Exception("Invalid inputs!");
+				throw new Exception("[NNLS]: Invalid inputs!");
 
 			// Inputs
-			int SIZE_OF_M_VECTOR = F.rows();
+			int size_of_m_vector = F.rows();
 
-			IntByReference mda = new IntByReference(SIZE_OF_M_VECTOR);
-			IntByReference m = new IntByReference(SIZE_OF_M_VECTOR); // mda == m
+			IntByReference mda = new IntByReference(size_of_m_vector);
+			IntByReference m = new IntByReference(size_of_m_vector); // mda == m
 			IntByReference n = new IntByReference(E.columns());
-			Memory a = new Memory(8 * mda.getValue() * n.getValue()); // 8 ==
-																		// sizeof(double)
-			double[] e = new double[SIZE_OF_M_VECTOR * (E.columns())];
-			double[] f = new double[SIZE_OF_M_VECTOR];
-			for (int i = 0; i < SIZE_OF_M_VECTOR; ++i)
+			Memory a = new Memory(SIZE_OF_DOUBLE * mda.getValue()
+					* n.getValue()); // 8 ==
+			// sizeof(double)
+			double[] e = new double[size_of_m_vector * (E.columns())];
+			double[] f = new double[size_of_m_vector];
+			for (int i = 0; i < size_of_m_vector; ++i)
 				f[i] = F.get(i);
 			int count = 0;
 			for (int i = 0; i < E.columns(); ++i)
@@ -58,18 +86,18 @@ public class LeastSquares {
 			a.write(0, e, 0, mda.getValue() * n.getValue()); // Fortran expects
 																// column-order
 																// array!
-			Memory b = new Memory(8 * mda.getValue());
+			Memory b = new Memory(SIZE_OF_DOUBLE * mda.getValue());
 			b.write(0, f, 0, mda.getValue());
 
 			// Outputs
-			Memory x = new Memory(8 * n.getValue());
+			Memory x = new Memory(SIZE_OF_DOUBLE * n.getValue());
 			DoubleByReference rnorm = new DoubleByReference();
 			IntByReference mode = new IntByReference();
 
 			// Temporary working arrays
-			Memory w = new Memory(8 * n.getValue());
-			Memory zz = new Memory(8 * m.getValue());
-			Memory index = new Memory(4 * n.getValue());
+			Memory w = new Memory(SIZE_OF_DOUBLE * n.getValue());
+			Memory zz = new Memory(SIZE_OF_DOUBLE * m.getValue());
+			Memory index = new Memory(SIZE_OF_INT * n.getValue());
 			NNLSLibrary.INSTANCE.nnls_(a, mda, m, n, b, x, rnorm, w, zz, index,
 					mode);
 			double[] res = new double[n.getValue()];
