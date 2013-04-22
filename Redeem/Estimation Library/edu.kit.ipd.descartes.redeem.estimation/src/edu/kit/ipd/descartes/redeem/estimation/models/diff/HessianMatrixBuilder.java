e5b1866da2a645e914aa26b37ff26493b3b177cb
spinners <spinners@9e42b895-fcda-4063-8a3b-11be15eb1bbd>
2013-04-22 12:49:15 +0000

    Add helper class to calculate hessian matrix.
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/descartes/code/redeem/trunk@11330 9e42b895-fcda-4063-8a3b-11be15eb1bbd

---
+++ Redeem/Estimation Library/edu.kit.ipd.descartes.redeem.estimation/src/edu/kit/ipd/descartes/redeem/estimation/models/diff/HessianMatrixBuilder.java
@@ -0,0 +1,26 @@
+package edu.kit.ipd.descartes.redeem.estimation.models.diff;
+
+import edu.kit.ipd.descartes.linalg.Matrix;
+import edu.kit.ipd.descartes.linalg.Vector;
+import edu.kit.ipd.descartes.redeem.estimation.models.observation.functions.IOutputFunction;
+import edu.kit.ipd.descartes.redeem.estimation.models.state.constraints.INonLinearConstraint;
+
+public final class HessianMatrixBuilder {
+	
+	public static Matrix calculateOfOutputFunction(IOutputFunction function, Vector x) {
+		if (function instanceof IDifferentiableFunction) {
+			return ((IDifferentiableFunction)function).getSecondDerivatives(x);
+		} else {
+			throw new IllegalStateException("Output function cannot be derived.");
+		}
+	}
+	
+	public static Matrix calculateOfConstraint(INonLinearConstraint constraint, Vector x) {
+		if (constraint instanceof IDifferentiableFunction) {
+			return ((IDifferentiableFunction)constraint).getSecondDerivatives(x);
+		} else {
+			throw new IllegalStateException("Constraint function cannot be derived.");
+		}
+	}
+
+}
