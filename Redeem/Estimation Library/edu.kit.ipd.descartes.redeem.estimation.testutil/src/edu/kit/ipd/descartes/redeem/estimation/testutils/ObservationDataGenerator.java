75e8ce40a96eb085cea53af0214bdee4e93ed1b6
spinners <spinners@9e42b895-fcda-4063-8a3b-11be15eb1bbd>
2013-04-22 12:53:48 +0000

    Add seperate testutil project for common test classes.
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/descartes/code/redeem/trunk@11339 9e42b895-fcda-4063-8a3b-11be15eb1bbd

---
+++ Redeem/Estimation Library/edu.kit.ipd.descartes.redeem.estimation.testutil/src/edu/kit/ipd/descartes/redeem/estimation/testutils/ObservationDataGenerator.java
@@ -0,0 +1,213 @@
+package edu.kit.ipd.descartes.redeem.estimation.testutils;
+
+import static edu.kit.ipd.descartes.linalg.LinAlg.*;
+
+import java.util.Arrays;
+import java.util.Random;
+
+import edu.kit.ipd.descartes.linalg.LinAlg;
+import edu.kit.ipd.descartes.linalg.Matrix;
+import edu.kit.ipd.descartes.linalg.MatrixFunction;
+import edu.kit.ipd.descartes.linalg.Scalar;
+import edu.kit.ipd.descartes.linalg.Vector;
+import edu.kit.ipd.descartes.linalg.VectorFunction;
+import edu.kit.ipd.descartes.redeem.estimation.repository.IMonitoringRepository;
+import edu.kit.ipd.descartes.redeem.estimation.repository.Query;
+import edu.kit.ipd.descartes.redeem.estimation.repository.Query.Type;
+import edu.kit.ipd.descartes.redeem.estimation.repository.Result;
+import edu.kit.ipd.descartes.redeem.estimation.system.IModelEntity;
+import edu.kit.ipd.descartes.redeem.estimation.system.Resource;
+import edu.kit.ipd.descartes.redeem.estimation.system.Service;
+import edu.kit.ipd.descartes.redeem.estimation.system.SystemModel;
+
+public class ObservationDataGenerator implements IMonitoringRepository {
+	
+	private Random randUtil;
+	private Random randWheights;
+	private Random randDemands;
+	private double upperUtilizationBound = 0.0;
+	private double lowerUtilizationBound = 1.0;
+	private Vector demands;
+	private double workloadMixVariation = 1.0;
+	
+	private Resource[] resources;
+	private Service[] services;
+	
+	private SystemModel model;
+	
+	private Observation currentObservation = null;
+	
+	public ObservationDataGenerator(long seed, int numWorkloadClasses, int numResources) {
+		Random randSeed = new Random(seed);
+		randUtil = new Random(randSeed.nextLong());
+		randWheights = new Random(randSeed.nextLong());
+		randDemands = new Random(randSeed.nextLong());
+		
+		services = new Service[numWorkloadClasses];
+		resources = new Resource[numResources];
+		for (int i = 0; i < numWorkloadClasses; i++) {
+			services[i] = new Service("WC" + i);
+		}
+		
+		for (int i = 0; i < numResources; i++) {
+			resources[i] = new Resource("R" + i);
+		}
+		
+		model = new SystemModel(Arrays.asList(resources), Arrays.asList(services));
+	}
+	
+	public double getUpperUtilizationBound() {
+		return upperUtilizationBound;
+	}
+	
+	public void setUpperUtilizationBound(double upperUtilizationBound) {
+		this.upperUtilizationBound = upperUtilizationBound;
+	}
+
+	public double getLowerUtilizationBound() {
+		return lowerUtilizationBound;
+	}
+	
+	public void setLowerUtilizationBound(double lowerUtilizationBound) {
+		this.lowerUtilizationBound = lowerUtilizationBound;
+	}
+
+	public double getWorkloadMixVariation() {
+		return workloadMixVariation;
+	}
+
+	public void setWorkloadMixVariation(double workloadMixVariation) {
+		this.workloadMixVariation = workloadMixVariation;
+	}	
+
+	public Vector getDemands() {
+		return demands;
+	}
+
+	public void setDemands(Vector demands) {
+		if (demands.rows() != services.length * resources.length) {
+			throw new IllegalArgumentException("Row count of demands does not match number of resources * services.");
+		}
+		this.demands = demands;
+	}
+	
+	public void setRandomDemands() {
+		demands = vector(resources.length * services.length, new VectorFunction() {			
+			@Override
+			public double cell(int row) {
+				return randDemands.nextDouble();
+			}
+		});
+	}
+	
+	public Observation nextObservation() {		
+		Vector absoluteWheights = vector(services.length, new VectorFunction() {			
+			@Override 
+			public double cell(int row) {
+				return 1.0 - randWheights.nextDouble() * workloadMixVariation;
+			}
+		});
+				
+		final Vector relativeWheights = absoluteWheights.times(sum(absoluteWheights));
+		
+		Vector weightedTotalDemand = vector(resources.length, new VectorFunction() {			
+			@Override
+			public double cell(int row) {
+				return relativeWheights.dot(demands.slice(model.getState().getRange(resources[row])));
+			}
+		});	
+		
+		final int bottleneckResource = max(weightedTotalDemand);
+		
+		final double maxUtil = randUtil.nextDouble() * (upperUtilizationBound - lowerUtilizationBound) + lowerUtilizationBound;
+		
+		final double totalThroughput = maxUtil / weightedTotalDemand.get(bottleneckResource);		
+		final Vector throughput = vector(services.length, new VectorFunction() {
+			@Override
+			public double cell(int row) {
+				return totalThroughput * relativeWheights.get(row);
+			}
+			
+		});		
+		
+		final Vector utilization = vector(resources.length, new VectorFunction() {			
+			@Override
+			public double cell(int row) {
+				if (row != bottleneckResource) {
+					return throughput.dot(demands.slice(model.getState().getRange(resources[row])));
+				} else {
+					return maxUtil;
+				}
+			}
+		});
+	
+		final Vector responsetime = vector(services.length, new VectorFunction() {
+			@Override
+			public double cell(int row) {
+				double sumRT = 0.0;
+				for (int r = 0; r < resources.length; r++) {
+					sumRT += demands.get(model.getState().getIndex(resources[r], services[row])) / (1 - utilization.get(r));
+				}
+				return sumRT;
+			}
+			
+		});
+		currentObservation = new Observation(utilization, throughput, responsetime);
+		
+		return currentObservation;
+	}
+	
+	public SystemModel getSystemModel() {
+		return model;
+	}
+
+	@Override
+	public <T extends Matrix> Result<T> execute(Query<T> query) {
+		switch(query.getMetric()) {
+		case RESPONSE_TIME:
+			if (query.getType() == Type.ALL_SERVICES) {
+				return new Result<T>((T) currentObservation.getMeanResponseTime(), services);
+			} else if (query.getType() == Type.SERVICE) {
+				return new Result<T>((T) LinAlg.scalar(currentObservation.getMeanResponseTime().get(getIndexOfService((Service)query.getEntity()))), 
+						new IModelEntity[] { query.getEntity() });
+			}
+			break;
+		case UTILIZATION:
+			if (query.getType() == Type.ALL_RESOURCES) {
+				return new Result<T>((T) currentObservation.getMeanUtilization(), resources);
+			} else if (query.getType() == Type.RESOURCE) {
+				return new Result<T>((T) LinAlg.scalar(currentObservation.getMeanUtilization().get(getIndexOfResource((Resource)query.getEntity()))), 
+						new IModelEntity[] { query.getEntity() });
+			}
+			break;
+		case THROUGHPUT:
+			if (query.getType() == Type.ALL_SERVICES) {
+				return new Result<T>((T) currentObservation.getMeanThroughput(), services);
+			} else if (query.getType() == Type.SERVICE) {
+				return new Result<T>((T) LinAlg.scalar(currentObservation.getMeanThroughput().get(getIndexOfService((Service)query.getEntity()))), 
+						new IModelEntity[] { query.getEntity() });
+			}
+			break;
+		}
+		return null;
+	}
+	
+	private int getIndexOfResource(Resource resource) {
+		for (int i = 0; i < resources.length; i++) {
+			if (resources[i].equals(resource)) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	
+	private int getIndexOfService(Service service) {
+		for (int i = 0; i < resources.length; i++) {
+			if (services[i].equals(service)) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+}
---
+++ Redeem/Estimation Library/edu.kit.ipd.descartes.redeem.estimation.testutil/src/edu/kit/ipd/descartes/redeem/estimation/testutils/ObservationDataGeneratorTest.java
@@ -0,0 +1,138 @@
+package edu.kit.ipd.descartes.redeem.estimation.testutils;
+
+import static edu.kit.ipd.descartes.linalg.LinAlg.*;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import edu.kit.ipd.descartes.linalg.Matrix;
+import edu.kit.ipd.descartes.linalg.Vector;
+
+public class ObservationDataGeneratorTest {
+	
+	private static final double EPSILON = 1e-6;
+	
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testNextObservation1WC1R() {
+		ObservationDataGenerator generator = new ObservationDataGenerator(42, 1, 1);
+		
+		Vector demands = vector(0.25);
+		generator.setDemands(demands);
+		
+		for (int i = 0; i < 10000; i++) {
+			assertObservation(generator.nextObservation(), demands);
+		}
+	}
+	
+	@Test
+	public void testNextObservation4WC1R() {
+		ObservationDataGenerator generator = new ObservationDataGenerator(42, 4, 1);
+		
+		Vector demands = vector(0.25, 0.3, 0.35, 0.4);
+		generator.setDemands(demands);
+		
+		for (int i = 0; i < 10000; i++) {
+			assertObservation(generator.nextObservation(), demands);
+		}
+	}
+	
+	
+	@Test
+	public void testNextObservation1WC4R() {
+		ObservationDataGenerator generator = new ObservationDataGenerator(42, 1, 4);
+		
+		Vector demands = vector(0.25, 0.3, 0.35, 0.4);
+		generator.setDemands(demands);
+		
+		for (int i = 0; i < 10000; i++) {
+			assertObservation(generator.nextObservation(), demands);
+		}
+	}
+	
+	@Test
+	public void testNextObservation4WC4R() {
+		ObservationDataGenerator generator = new ObservationDataGenerator(42, 4, 4);
+		
+		Vector demands = vector(
+				0.25, 0.3, 0.35, 0.4,
+				0.4, 0.25, 0.3, 0.35,
+				0.35, 0.4, 0.25, 0.3,
+				0.3, 0.35, 0.4, 0.25);
+		generator.setDemands(demands);
+		
+		for (int i = 0; i < 10000; i++) {
+			assertObservation(generator.nextObservation(), demands);
+		}
+	}
+	
+	@Test
+	public void testNextObservation4WC1RWithBounds() {
+		ObservationDataGenerator generator = new ObservationDataGenerator(42, 4, 1);
+		generator.setLowerUtilizationBound(0.4);
+		generator.setUpperUtilizationBound(0.6);
+		
+		Vector demands = vector(
+				0.25,
+				0.3,
+				0.35,
+				0.4);
+		generator.setDemands(demands);
+		
+		for (int i = 0; i < 10000; i++) {
+			Observation ob = generator.nextObservation();
+			assertTrue(ob.getMeanUtilization().get(0) >= 0.4);
+			assertTrue(ob.getMeanUtilization().get(0) <= 0.6);
+		}
+	}
+	
+	@Test
+	public void testNextObservation4WC1RWithNoMix() {
+		ObservationDataGenerator generator = new ObservationDataGenerator(42, 4, 1);
+		generator.setWorkloadMixVariation(0);
+		
+		Vector demands = vector(
+				0.25,
+				0.3,
+				0.35,
+				0.4);
+		generator.setDemands(demands);
+		
+		for (int i = 0; i < 10000; i++) {
+			Observation ob = generator.nextObservation();
+			for (int w = 1; w < 4; w++) {
+				assertEquals(ob.getMeanThroughput().get(w-1), ob.getMeanThroughput().get(w), EPSILON);
+			}
+		}
+	}
+	
+	private void assertObservation(Observation ob, Matrix demands) {
+		for (int r = 0; r < demands.columns(); r++) {
+			double util = demands.column(r).dot(ob.getMeanThroughput());
+			assertEquals(util, ob.getMeanUtilization().get(r), EPSILON);
+		}
+		
+		for (int i = 0; i < demands.rows(); i++) {
+			double sumRT = 0.0;
+			for (int r = 0; r < demands.columns(); r++) {
+				sumRT += demands.get(i, r) / (1 - demands.column(r).dot(ob.getMeanThroughput()));
+			}
+			assertEquals(sumRT, ob.getMeanResponseTime().get(i), EPSILON);
+		}
+	}
+	
+
+}
