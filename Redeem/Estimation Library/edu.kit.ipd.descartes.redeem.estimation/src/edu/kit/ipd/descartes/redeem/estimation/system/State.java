ddb1f36e0b0a07d0cccf98fe8e82a0d8d8adb353
spinners <spinners@9e42b895-fcda-4063-8a3b-11be15eb1bbd>
2013-04-22 12:48:55 +0000

    Fixed several shortcomings of current Resource, Service, State and StateModel classes.
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/descartes/code/redeem/trunk@11329 9e42b895-fcda-4063-8a3b-11be15eb1bbd

--- Redeem/Estimation Library/edu.kit.ipd.descartes.redeem.estimation/src/edu/kit/ipd/descartes/redeem/estimation/system/State.java
+++ Redeem/Estimation Library/edu.kit.ipd.descartes.redeem.estimation/src/edu/kit/ipd/descartes/redeem/estimation/system/State.java
@@ -1,23 +1,45 @@
 package edu.kit.ipd.descartes.redeem.estimation.system;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import edu.kit.ipd.descartes.linalg.Range;
 
 public class State {
 	
-	public Range getRange(Resource resource) {
-		return null;
+	private Map<Resource, Range> ranges = new HashMap<>();
+	private Map<Service, Integer> serviceIndex = new HashMap<>();
+	private List<Resource> resources;
+	private List<Service> services;
+	
+	State(List<Resource> resources, List<Service> services) {
+		this.services = services;
+		this.resources = resources;
+		for (int i = 0; i < services.size(); i++) {
+			serviceIndex.put(services.get(i), i);
+		}
+		for (int i = 0; i < resources.size(); i++) {
+			Range r = new Range(i * services.size(), (i + 1) * services.size());
+			ranges.put(resources.get(i), r);
+		}
 	}
 	
-	public int getIndex(Resource resource, Service cls) {
-		return 0;
+	public Range getRange(Resource resource) {
+		return ranges.get(resource);
+	}
+	
+	public int getIndex(Resource resource, Service service) {		
+		Range r = ranges.get(resource);
+		return r.getStart() + serviceIndex.get(service);
 	}
 	
 	public Resource getResource(int index) {
-		return null;
+		return resources.get(index / services.size());
 	}
 	
-	public Service getWorkloadClass(int index) {
-		return null;
+	public Service getService(int index) {
+		return services.get(index % services.size());
 	}
 
 }
--- Redeem/Estimation Library/edu.kit.ipd.descartes.redeem.estimation/src/edu/kit/ipd/descartes/redeem/estimation/system/SystemModel.java
+++ Redeem/Estimation Library/edu.kit.ipd.descartes.redeem.estimation/src/edu/kit/ipd/descartes/redeem/estimation/system/SystemModel.java
@@ -5,23 +5,27 @@ import java.util.List;
 
 public class SystemModel {
 	
-	private List<Resource> resources;
+	private final List<Resource> resources;
 	
-	private List<Service> workloadClasses;
+	private final List<Service> services;
+	
+	private final State state;
 
-	public SystemModel() {
-
+	public SystemModel(List<Resource> resources, List<Service> services) {
+		this.resources = resources;
+		this.services = services;
+		this.state = new State(resources, services);
 	}
 
 	public List<Resource> getResources() {
 		return Collections.unmodifiableList(resources);
 	}
 
-	public List<Service> getWorkloadClasses() {
-		return Collections.unmodifiableList(workloadClasses);
+	public List<Service> getServices() {
+		return Collections.unmodifiableList(services);
 	}
 	
 	public State getState() {
-		return null;
+		return state;
 	}
 }
