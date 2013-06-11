cfcd97f54f96149e395fc85d08047ec5f86801ad
spinners <spinners@9e42b895-fcda-4063-8a3b-11be15eb1bbd>
2013-06-11 14:05:33 +0000

    Implement AbstractDirectOutputFunction completely.
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/descartes/code/redeem/trunk@12026 9e42b895-fcda-4063-8a3b-11be15eb1bbd

--- Redeem/Estimation Library/edu.kit.ipd.descartes.redeem.estimation/src/edu/kit/ipd/descartes/redeem/estimation/models/observation/functions/AbstractDirectOutputFunction.java
+++ Redeem/Estimation Library/edu.kit.ipd.descartes.redeem.estimation/src/edu/kit/ipd/descartes/redeem/estimation/models/observation/functions/AbstractDirectOutputFunction.java
@@ -1,24 +1,31 @@
 package edu.kit.ipd.descartes.redeem.estimation.models.observation.functions;
 
-import java.util.List;
+import java.util.Arrays;
 
+import edu.kit.ipd.descartes.linalg.LinAlg;
 import edu.kit.ipd.descartes.linalg.Vector;
 import edu.kit.ipd.descartes.redeem.estimation.system.Resource;
-import edu.kit.ipd.descartes.redeem.estimation.system.SystemModel;
 import edu.kit.ipd.descartes.redeem.estimation.system.Service;
+import edu.kit.ipd.descartes.redeem.estimation.system.SystemModel;
 
 public abstract class AbstractDirectOutputFunction extends
 		AbstractLinearOutputFunction implements IDirectOutputFunction {
+	
+	private Vector zeros;
+	private int idx;
 
 	protected AbstractDirectOutputFunction(SystemModel system,
-			List<Resource> selectedResources,
-			List<Service> selectedClasses) {
-		super(system, selectedResources, selectedClasses);
+			Resource resource,
+			Service service) {
+		super(system, Arrays.asList(resource), Arrays.asList(service));
+		
+		zeros = LinAlg.zeros(getSystem().getState().getStateSize());
+		idx = getSystem().getState().getIndex(resource, service);
 	}
 
 	@Override
 	public Vector getIndependentVariables() {
-		return null;
+		return zeros.set(idx, getFactor());
 	}
 
 }
