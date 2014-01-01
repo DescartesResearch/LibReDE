0400b1193e2d1972d637ca421f57c1a435c07fac
Simon Spoinner <simon.spinner@kit.edu>
2014-01-01 17:47:35 +0100

    Matlab scripts: initialize repo data from matlab array

--- Redeem/Estimation Library/edu.kit.ipd.descartes.redeem.matlab/matlab/load_classes.m
+++ Redeem/Estimation Library/edu.kit.ipd.descartes.redeem.matlab/matlab/load_classes.m
@@ -3,7 +3,8 @@ function [] = load_classes()
 persistent loaded;
 
 if isempty(loaded)
-    jars = dir('../lib/plugins/*.jar');
+    path = fileparts(mfilename('fullpath'));
+    jars = dir(strcat(path, '/../lib/plugins/*.jar'));
     dpath = javaclasspath('-dynamic');
     for i = 1:size(jars,1)
         if isempty(strfind(dpath, jars(i).name))
