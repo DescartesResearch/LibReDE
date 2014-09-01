813710397394503e76dd197869c672b0bbeeb0be
Simon Spinner <simon.spinner@uni-wuerzburg.de>
2014-09-01 16:56:32 +0200

    Remove ui project (merged with generated EMF editor)

--- edu.kit.ipd.descartes.librede.ui/src/edu/kit/ipd/descartes/librede/ui/LibredeUIPlugin.java
+++ edu.kit.ipd.descartes.librede.ui/src/edu/kit/ipd/descartes/librede/ui/LibredeUIPlugin.java
@@ -1,21 +1,85 @@
 package edu.kit.ipd.descartes.librede.ui;
 
-import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.eclipse.emf.common.EMFPlugin;
+import org.eclipse.emf.common.ui.EclipseUIPlugin;
+import org.eclipse.emf.common.util.ResourceLocator;
 
 
-public class LibredeUIPlugin extends AbstractUIPlugin {
+public class LibredeUIPlugin extends EMFPlugin {
 	
 	public static final String PLUGIN_ID = "edu.kit.ipdate.descartes.librede.ui";
 
-	// shared instance
-	private static LibredeUIPlugin instance;
+	/**
+	 * Keep track of the singleton.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static final LibredeUIPlugin INSTANCE = new LibredeUIPlugin();
 	
+	/**
+	 * Keep track of the singleton.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	private static Implementation plugin;
+
+	/**
+	 * Create the instance.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
 	public LibredeUIPlugin() {
-		instance = this;
+		super
+			(new ResourceLocator [] {
+			});
+	}
+
+	/**
+	 * Returns the singleton instance of the Eclipse plugin.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return the singleton instance.
+	 * @generated
+	 */
+	@Override
+	public ResourceLocator getPluginResourceLocator() {
+		return plugin;
 	}
 	
-	public static LibredeUIPlugin getDefault() {
-		return instance;
+	/**
+	 * Returns the singleton instance of the Eclipse plugin.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return the singleton instance.
+	 * @generated
+	 */
+	public static Implementation getPlugin() {
+		return plugin;
+	}
+	
+	/**
+	 * The actual implementation of the Eclipse <b>Plugin</b>.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static class Implementation extends EclipseUIPlugin {
+		/**
+		 * Creates an instance.
+		 * <!-- begin-user-doc -->
+		 * <!-- end-user-doc -->
+		 * @generated
+		 */
+		public Implementation() {
+			super();
+	
+			// Remember the static instance.
+			//
+			plugin = this;
+		}
 	}
 
 }
