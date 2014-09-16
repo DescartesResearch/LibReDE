832d9de8c1736e8d64f3b2d8de29820ef5798386
Simon Spinner <simon.spinner@uni-wuerzburg.de>
2014-09-16 11:44:08 +0200

    Initial implementation of csv exporter.

--- edu.kit.ipd.descartes.librede.estimation/src/edu/kit/ipd/descartes/librede/export/IExporter.java
+++ edu.kit.ipd.descartes.librede.estimation/src/edu/kit/ipd/descartes/librede/export/IExporter.java
@@ -1,7 +1,9 @@
 package edu.kit.ipd.descartes.librede.export;
 
-import edu.kit.ipd.descartes.librede.configuration.Component;
+import edu.kit.ipd.descartes.librede.estimation.repository.TimeSeries;
 
 public interface IExporter {
+	
+	public void writeResults(String approach, int fold, TimeSeries estimates) throws Exception;
 
 }
