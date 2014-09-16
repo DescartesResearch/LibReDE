af37bdb64d5c2a5da0f096e6500f9fc03ec71349
Simon Spinner <simon.spinner@uni-wuerzburg.de>
2014-09-16 11:38:24 +0200

    Add implementation for CSV data source.

--- edu.kit.ipd.descartes.librede.estimation/src/edu/kit/ipd/descartes/librede/datasource/IDataItem.java
+++
@@ -1,7 +0,0 @@
-package edu.kit.ipd.descartes.librede.datasource;
-
-import edu.kit.ipd.descartes.librede.configuration.Component;
-
-public interface IDataItem {
-
-}
--- edu.kit.ipd.descartes.librede.estimation/src/edu/kit/ipd/descartes/librede/datasource/IDataSource.java
+++ edu.kit.ipd.descartes.librede.estimation/src/edu/kit/ipd/descartes/librede/datasource/IDataSource.java
@@ -1,5 +1,11 @@
 package edu.kit.ipd.descartes.librede.datasource;
 
+import java.io.InputStream;
+
+import edu.kit.ipd.descartes.librede.estimation.repository.TimeSeries;
+
 public interface IDataSource {
+	
+	public TimeSeries load(InputStream in, int column) throws Exception;
 
 }
