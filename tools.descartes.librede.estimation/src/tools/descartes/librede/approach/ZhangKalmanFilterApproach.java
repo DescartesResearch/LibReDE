aba26ab5476ad1980e5d0ac34471b05999f0feb5
Simon Spinner <simon.spinner@uni-wuerzburg.de>
2014-11-12 13:35:36 +0100

    Port ZhangKalmanFilter to new format.

--- tools.descartes.librede.estimation/src/tools/descartes/librede/approach/ZhangKalmanFilterApproach.java
+++ tools.descartes.librede.estimation/src/tools/descartes/librede/approach/ZhangKalmanFilterApproach.java
@@ -24,67 +24,67 @@
  * [Java is a trademark or registered trademark of Sun Microsystems, Inc.
  * in the United States and other countries.]
  */
-package tools.descartes.librede.approaches;
+package tools.descartes.librede.approach;
 
 import static tools.descartes.librede.linalg.LinAlg.ones;
-import tools.descartes.librede.approach.AbstractEstimationApproach;
-import tools.descartes.librede.bayesplusplus.ExtendedKalmanFilter;
+
+import java.util.Arrays;
+import java.util.List;
+
+import tools.descartes.librede.algorithm.EstimationAlgorithmFactory;
+import tools.descartes.librede.algorithm.IEstimationAlgorithm;
+import tools.descartes.librede.algorithm.IKalmanFilterAlgorithm;
 import tools.descartes.librede.configuration.Resource;
 import tools.descartes.librede.configuration.Service;
-import tools.descartes.librede.exceptions.InitializationException;
+import tools.descartes.librede.models.observation.IObservationModel;
 import tools.descartes.librede.models.observation.VectorObservationModel;
 import tools.descartes.librede.models.observation.functions.IOutputFunction;
 import tools.descartes.librede.models.observation.functions.ResponseTimeEquation;
 import tools.descartes.librede.models.observation.functions.UtilizationLaw;
 import tools.descartes.librede.models.state.ConstantStateModel;
+import tools.descartes.librede.models.state.ConstantStateModel.Builder;
+import tools.descartes.librede.models.state.IStateModel;
 import tools.descartes.librede.models.state.constraints.Unconstrained;
 import tools.descartes.librede.registry.Component;
-import tools.descartes.librede.registry.ParameterDefinition;
 import tools.descartes.librede.repository.IRepositoryCursor;
 import tools.descartes.librede.workload.WorkloadDescription;
 
 @Component(displayName = "Kalman Filter using Response Times and Utilization")
 public class ZhangKalmanFilterApproach extends AbstractEstimationApproach {
-	
-	public static final String NAME = "ZhangKalmanFilter";
-	
-	@ParameterDefinition(name = "StateNoiseCovariance", label = "State Noise Covariance", defaultValue = "1.0")
-	private double stateNoiseCovariance;
-	
-	@ParameterDefinition(name = "StateNoiseCoupling", label = "State Noise Coupling", defaultValue = "1.0")
-	private double stateNoiseCoupling;
-	
-	@ParameterDefinition(name = "ObserveNoiseCovariance", label = "Observe Noise Covariance", defaultValue = "0.0001")
-	private double observeNoiseCovariance;
 
 	@Override
-	public void initialize(WorkloadDescription workload,
-			IRepositoryCursor cursor, int estimationWindow, boolean iterative)
-			throws InitializationException {
-		super.initialize(workload, cursor, estimationWindow, iterative);
-		
-		int stateSize = workload.getServices().size();
-		
-		ConstantStateModel<Unconstrained> stateModel = new ConstantStateModel<Unconstrained>(stateSize, ones(stateSize).times(0.01));
-		
-		VectorObservationModel<IOutputFunction> observationModel = new VectorObservationModel<IOutputFunction>();
-		for (Service serv : workload.getServices()) {
-			ResponseTimeEquation func = new ResponseTimeEquation(workload, cursor, serv, workload.getResources());
-			observationModel.addOutputFunction(func);
-		}
-		
-		
+	protected List<IStateModel<?>> deriveStateModels(
+			WorkloadDescription workload, IRepositoryCursor cursor) {
+		Builder<Unconstrained> b = ConstantStateModel.unconstrainedModelBuilder();
 		for (Resource res : workload.getResources()) {
-			UtilizationLaw func = new UtilizationLaw(workload, cursor, res);
+			for (Service serv : workload.getServices()) {
+				b.addVariable(res, serv);
+			}
+		}
+		b.setInitialState(ones(workload.getResources().size() * workload.getServices().size()).times(0.01));
+		return Arrays.asList(b.build());
+	}
+
+	@Override
+	protected IObservationModel<?, ?> deriveObservationModel(
+			IStateModel<?> stateModel, IRepositoryCursor cursor) {
+		VectorObservationModel<IOutputFunction> observationModel = new VectorObservationModel<IOutputFunction>();
+		for (Service serv : stateModel.getServices()) {
+			ResponseTimeEquation func = new ResponseTimeEquation(stateModel, cursor, serv, stateModel.getResources());
+			observationModel.addOutputFunction(func);
+		}	
+		
+		for (Resource res : stateModel.getResources()) {
+			UtilizationLaw func = new UtilizationLaw(stateModel, cursor, res);
 			observationModel.addOutputFunction(func);
 		}
+		return observationModel;
+	}
 
-		ExtendedKalmanFilter estimator = new ExtendedKalmanFilter();
-		estimator.setStateNoiseCouplingConstant(stateNoiseCoupling);
-		estimator.setStateNoiseCovarianceConstant(stateNoiseCovariance);
-		estimator.setObserveNoiseConstant(observeNoiseCovariance);
-		estimator.initialize(stateModel, observationModel, estimationWindow);
-		setEstimationAlgorithm(estimator);
+	@Override
+	protected IEstimationAlgorithm getEstimationAlgorithm(
+			EstimationAlgorithmFactory factory) {
+		return factory.createInstance(IKalmanFilterAlgorithm.class);
 	}
 
 
