86ad6a2d79522ebc5a805c48efe8d06b02ffcf7e
Simon Spinner <simon.spinner@uni-wuerzburg.de>
2014-11-16 21:44:15 +0100

    Change validators to use workload description as input.

--- tools.descartes.librede.estimation/src/tools/descartes/librede/validation/ResponseTimeValidator.java
+++ tools.descartes.librede.estimation/src/tools/descartes/librede/validation/ResponseTimeValidator.java
@@ -29,12 +29,16 @@ package tools.descartes.librede.validation;
 import java.util.ArrayList;
 import java.util.List;
 
+import tools.descartes.librede.configuration.Resource;
 import tools.descartes.librede.configuration.Service;
+import tools.descartes.librede.configuration.WorkloadDescription;
 import tools.descartes.librede.linalg.LinAlg;
 import tools.descartes.librede.linalg.MatrixBuilder;
 import tools.descartes.librede.linalg.Vector;
 import tools.descartes.librede.models.observation.functions.ResponseTimeEquation;
-import tools.descartes.librede.models.state.IStateModel;
+import tools.descartes.librede.models.state.ConstantStateModel;
+import tools.descartes.librede.models.state.ConstantStateModel.Builder;
+import tools.descartes.librede.models.state.constraints.Unconstrained;
 import tools.descartes.librede.registry.Component;
 import tools.descartes.librede.repository.IRepositoryCursor;
 
@@ -44,7 +48,15 @@ public class ResponseTimeValidator implements IValidator {
 	private List<ResponseTimeEquation> respEq;
 	private MatrixBuilder allErrors;
 	
-	public ResponseTimeValidator(IStateModel<?> stateModel, IRepositoryCursor cursor) {
+	public ResponseTimeValidator(WorkloadDescription workload, IRepositoryCursor cursor) {
+		Builder<Unconstrained> builder = ConstantStateModel.unconstrainedModelBuilder();
+		for (Resource res : workload.getResources()) {
+			for (Service serv : workload.getServices()) {
+				builder.addVariable(res, serv);
+			}
+		}
+		ConstantStateModel<Unconstrained> stateModel = builder.build(); 
+		
 		this.respEq = new ArrayList<ResponseTimeEquation>();
 		for (Service srv : stateModel.getServices()) {
 			respEq.add(new ResponseTimeEquation(stateModel, cursor, srv));
--- tools.descartes.librede.estimation/src/tools/descartes/librede/validation/UtilizationValidator.java
+++ tools.descartes.librede.estimation/src/tools/descartes/librede/validation/UtilizationValidator.java
@@ -30,11 +30,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 import tools.descartes.librede.configuration.Resource;
+import tools.descartes.librede.configuration.Service;
+import tools.descartes.librede.configuration.WorkloadDescription;
 import tools.descartes.librede.linalg.LinAlg;
 import tools.descartes.librede.linalg.MatrixBuilder;
 import tools.descartes.librede.linalg.Vector;
 import tools.descartes.librede.models.observation.functions.UtilizationLaw;
+import tools.descartes.librede.models.state.ConstantStateModel;
 import tools.descartes.librede.models.state.IStateModel;
+import tools.descartes.librede.models.state.ConstantStateModel.Builder;
+import tools.descartes.librede.models.state.constraints.Unconstrained;
 import tools.descartes.librede.registry.Component;
 import tools.descartes.librede.repository.IRepositoryCursor;
 
@@ -44,7 +49,15 @@ public class UtilizationValidator implements IValidator {
 	private List<UtilizationLaw> utilLaw;
 	private MatrixBuilder allErrors;
 	
-	public UtilizationValidator(IStateModel<?> stateModel, IRepositoryCursor cursor) {
+	public UtilizationValidator(WorkloadDescription workload, IRepositoryCursor cursor) {
+		Builder<Unconstrained> builder = ConstantStateModel.unconstrainedModelBuilder();
+		for (Resource res : workload.getResources()) {
+			for (Service serv : workload.getServices()) {
+				builder.addVariable(res, serv);
+			}
+		}
+		ConstantStateModel<Unconstrained> stateModel = builder.build(); 
+		
 		this.utilLaw = new ArrayList<UtilizationLaw>();
 		for (Resource res : stateModel.getResources()) {
 			utilLaw.add(new UtilizationLaw(stateModel, cursor, res));
