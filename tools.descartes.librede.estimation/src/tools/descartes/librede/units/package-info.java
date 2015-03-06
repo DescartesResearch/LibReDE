299309529a2ac11e5f2793d71301ebcf71032323
Simon Spinner <simon.spinner@uni-wuerzburg.de>
2015-03-06 15:52:17 +0100

    Add package description.

---
+++ tools.descartes.librede.estimation/src/tools/descartes/librede/units/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This package provides classes describing the different units of the input measurement data. 
+ * These classes are used throughout LibReDE to enforce compatible physical units and automatically 
+ * convert between units if necessary.
+ */
+package tools.descartes.librede.units;
