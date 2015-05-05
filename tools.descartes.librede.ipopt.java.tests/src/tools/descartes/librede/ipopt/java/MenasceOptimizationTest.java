0b3d04b6545667530b54e0b8ae76c1e180993797
Simon Spinner <simon.spinner@uni-wuerzburg.de>
2015-05-05 15:20:38 +0200

    Remove debug code from test.

--- tools.descartes.librede.ipopt.java.tests/src/tools/descartes/librede/ipopt/java/MenasceOptimizationTest.java
+++ tools.descartes.librede.ipopt.java.tests/src/tools/descartes/librede/ipopt/java/MenasceOptimizationTest.java
@@ -111,7 +111,6 @@ public class MenasceOptimizationTest extends LibredeTest {
 	public void testFiveServicesOneResource() throws Exception {
 		final ObservationDataGenerator generator = new ObservationDataGenerator(42, 5, 1);
 
-		System.in.read();
 		Vector demands = vector(0.03, 0.04, 0.05, 0.06, 0.07);
 		generator.setDemands(demands);
 		generator.setUpperUtilizationBound(0.9);
