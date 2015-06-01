0dba46f3657b3938bca4c511b56d10e0072a4572
Simon Spinner <simon.spinner@uni-wuerzburg.de>
2015-06-01 09:22:42 +0200

    Remove debug code from LiuOptimizationTest

--- tools.descartes.librede.ipopt.java.tests/src/tools/descartes/librede/ipopt/java/LiuOptimizationTest.java
+++ tools.descartes.librede.ipopt.java.tests/src/tools/descartes/librede/ipopt/java/LiuOptimizationTest.java
@@ -120,8 +120,6 @@ public class LiuOptimizationTest extends LibredeTest {
 	@Test
 	public void testFiveServicesOneResource() throws Exception {
 		final ObservationDataGenerator generator = new ObservationDataGenerator(42, 5, 1);
-		
-		System.in.read();
 
 		Vector demands = vector(0.03, 0.04, 0.05, 0.06, 0.07);
 		generator.setDemands(demands);
