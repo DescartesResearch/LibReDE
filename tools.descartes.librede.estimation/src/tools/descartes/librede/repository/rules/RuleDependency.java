83638afbdf2bd924591692e59477bd192b013fe4
Simon Spinner <simon.spinner@uni-wuerzburg.de>
2015-07-26 16:23:40 +0200

    Add classes for specifying derivation and aggregation rules.

---
+++ tools.descartes.librede.estimation/src/tools/descartes/librede/repository/rules/RuleDependency.java
@@ -0,0 +1,51 @@
+/**
+ * ==============================================
+ *  LibReDE : Library for Resource Demand Estimation
+ * ==============================================
+ *
+ * (c) Copyright 2013-2014, by Simon Spinner and Contributors.
+ *
+ * Project Info:   http://www.descartes-research.net/
+ *
+ * All rights reserved. This software is made available under the terms of the
+ * Eclipse Public License (EPL) v1.0 as published by the Eclipse Foundation
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * This software is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the Eclipse Public License (EPL)
+ * for more details.
+ *
+ * You should have received a copy of the Eclipse Public License (EPL)
+ * along with this software; if not visit http://www.eclipse.org or write to
+ * Eclipse Foundation, Inc., 308 SW First Avenue, Suite 110, Portland, 97204 USA
+ * Email: license (at) eclipse.org
+ *
+ * [Java is a trademark or registered trademark of Sun Microsystems, Inc.
+ * in the United States and other countries.]
+ */
+package tools.descartes.librede.repository.rules;
+
+import tools.descartes.librede.metrics.Aggregation;
+import tools.descartes.librede.metrics.Metric;
+import tools.descartes.librede.units.Dimension;
+
+public class RuleDependency<D extends Dimension> {
+	
+	private final Metric<D> metric;
+	private final Aggregation aggregation;	
+	
+	public RuleDependency(Metric<D> metric, Aggregation aggregation) {
+		super();
+		this.metric = metric;
+		this.aggregation = aggregation;
+	}
+
+	public Metric<D> getMetric() {
+		return metric;
+	}
+
+	public Aggregation getAggregation() {
+		return aggregation;
+	}
+}
---
+++ tools.descartes.librede.estimation/src/tools/descartes/librede/repository/rules/RulesConfig.java
@@ -0,0 +1,109 @@
+/**
+ * ==============================================
+ *  LibReDE : Library for Resource Demand Estimation
+ * ==============================================
+ *
+ * (c) Copyright 2013-2014, by Simon Spinner and Contributors.
+ *
+ * Project Info:   http://www.descartes-research.net/
+ *
+ * All rights reserved. This software is made available under the terms of the
+ * Eclipse Public License (EPL) v1.0 as published by the Eclipse Foundation
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * This software is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the Eclipse Public License (EPL)
+ * for more details.
+ *
+ * You should have received a copy of the Eclipse Public License (EPL)
+ * along with this software; if not visit http://www.eclipse.org or write to
+ * Eclipse Foundation, Inc., 308 SW First Avenue, Suite 110, Portland, 97204 USA
+ * Email: license (at) eclipse.org
+ *
+ * [Java is a trademark or registered trademark of Sun Microsystems, Inc.
+ * in the United States and other countries.]
+ */
+package tools.descartes.librede.repository.rules;
+
+import java.util.Collections;
+import java.util.EnumMap;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import tools.descartes.librede.metrics.Aggregation;
+import tools.descartes.librede.metrics.Metric;
+import tools.descartes.librede.units.Dimension;
+
+public class RulesConfig {
+	private final Map<Metric<?>, EnumMap<Aggregation, List<DerivationRule<?>>>> derivationRules = new HashMap<>();
+	private final Map<Metric<?>, EnumMap<Aggregation, List<AggregationRule<?>>>> aggregationRules = new HashMap<>();
+	private final List<AggregationRule<?>> defaultRules = new LinkedList<>();
+	
+	public void addDerivationRule(DerivationRule<?> rule) {
+		if (rule.getDependencies().isEmpty()) {
+			throw new IllegalArgumentException();
+		} else {
+			for (RuleDependency<?> r : rule.getDependencies()) {
+				EnumMap<Aggregation, List<DerivationRule<?>>> metricEntry = derivationRules.get(r.getMetric());
+				if (metricEntry == null) {
+					metricEntry = new EnumMap<>(Aggregation.class);
+					derivationRules.put(r.getMetric(), metricEntry);
+				}
+				List<DerivationRule<?>> aggregationEntry = metricEntry.get(r.getAggregation());
+				if (aggregationEntry == null) {
+					aggregationEntry = new LinkedList<>();
+					metricEntry.put(r.getAggregation(), aggregationEntry);
+				}
+				aggregationEntry.add(rule);
+			}
+		}
+	}
+	
+	public void addAggregationRule(AggregationRule<?> rule) {
+		if (rule.getDependencies().isEmpty()) {
+			defaultRules.add(rule);
+		}
+		for (RuleDependency<?> r : rule.getDependencies()) {
+			EnumMap<Aggregation, List<AggregationRule<?>>> metricEntry = aggregationRules.get(r.getMetric());
+			if (metricEntry == null) {
+				metricEntry = new EnumMap<>(Aggregation.class);
+				aggregationRules.put(r.getMetric(), metricEntry);
+			}
+			List<AggregationRule<?>> aggregationEntry = metricEntry.get(r.getAggregation());
+			if (aggregationEntry == null) {
+				aggregationEntry = new LinkedList<>();
+				metricEntry.put(r.getAggregation(), aggregationEntry);
+			}
+			aggregationEntry.add(rule);
+		}
+	}
+	
+	public List<AggregationRule<?>> getAggregationRules(Metric<?> metric, Aggregation aggregation) {
+		EnumMap<Aggregation, List<AggregationRule<?>>> metricEntry = aggregationRules.get(metric);
+		if (metricEntry != null) {
+			List<AggregationRule<?>> aggregationEntry = metricEntry.get(aggregation);
+			if (aggregationEntry != null) {
+				return aggregationEntry;
+			}
+		}
+		return Collections.emptyList();
+	}
+	
+	public List<DerivationRule<?>> getDerivationRules(Metric<?> metric, Aggregation aggregation) {
+		EnumMap<Aggregation, List<DerivationRule<?>>> metricEntry = derivationRules.get(metric);
+		if (metricEntry != null) {
+			List<DerivationRule<?>> derivationEntry = metricEntry.get(aggregation);
+			if (derivationEntry != null) {
+				return derivationEntry;
+			}
+		}
+		return Collections.emptyList();
+	}
+	
+	public List<AggregationRule<?>> getDefaultDerivationRules() {
+		return defaultRules;
+	}
+}
