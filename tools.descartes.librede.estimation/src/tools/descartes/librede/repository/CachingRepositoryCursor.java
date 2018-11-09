5ab368f5bced01aaa7a35ec84c667fc20ae68cbb
Johannes Grohmann <johannes.grohmann@uni-wuerzburg.de>
2018-11-09 11:20:55 +0100

    removed warning message

--- tools.descartes.librede.estimation/src/tools/descartes/librede/repository/CachingRepositoryCursor.java
+++ tools.descartes.librede.estimation/src/tools/descartes/librede/repository/CachingRepositoryCursor.java
@@ -191,9 +191,9 @@ public class CachingRepositoryCursor implements IRepositoryCursor {
 			entry.moveTo(lastInterval);
 			if (!entry.contains(interval)) {
 				double value = delegate.getAggregatedValue(interval, metric, unit, entity, func);
-				if(Double.isNaN(value)){
-					System.out.println("We got not values for this combination. Fix the start timestamp perhaps...");
-				}
+//				if(Double.isNaN(value)){
+//					log.warn("We got no values for this combination. Fix the start timestamp perhaps...");
+//				}
 				entry.set(interval, value);
 				if (log.isTraceEnabled()) {
 					log.trace("Set cache entry (interval=" + interval + ", metric=" + metric.getName() + ", unit=" + unit.toString() + ", entity=" + entity.getName() + ", func=" + func.getName() + ", value=" + value + ")");
